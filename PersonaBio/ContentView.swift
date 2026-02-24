aa4413fa7ee8af9b1cdd565fe2c0e5b08258206f
dfy37 <1356717485@qq.com>
2026-02-24 11:09:32 +0800

    Initial Commit

---
+++ PersonaBio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonaBio
+//
+//  Created by DUAN Feiyu on 2026/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonaBio/PersonaBioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonaBioApp.swift
+//  PersonaBio
+//
+//  Created by DUAN Feiyu on 2026/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PersonaBioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
